6777b6709db888f6ce0367506b1de9dcd6439663
Henry <henry@reed.edu>
2019-04-19 21:43:13 -0700

    fixed shading

--- scene.js
+++ scene.js
@@ -1,6 +1,5 @@
 const THREE = require('three');
 const ConvexGeometry = require('./ConvexGeometry.js');
-const test = require('./test.js')
 
 const defaultPoint = new THREE.Vector3(1, 0, 0);
 
@@ -150,6 +149,14 @@ function addPoint() {
 	}
 }
 
+function addInsetLine(points, color) {
+	var lineGeometry = new THREE.Geometry()
+	for (var i = 0; i < points.length; i++) { lineGeometry.vertices.push(points[i]) }
+	const lineMaterial = new THREE.LineBasicMaterial({ color: color, linewidth: 4 })
+	const line = new THREE.Line(lineGeometry, lineMaterial)
+	inset.add(line)
+}
+
 function addInsetPointAt(point) {
 	// Add new point
 	const addedPointMaterial = new THREE.MeshBasicMaterial({color: pointToColor(point)});
@@ -168,10 +175,17 @@ const fiberMaterial = new THREE.LineBasicMaterial({color: 0xff0000, linewidth: 5
 const newFiber = new THREE.Line(fiberGeo, fiberMaterial);
 scene.add(newFiber);
 
-// Lighting
-const light = new THREE.PointLight(0xffffff);
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(light);
+var light = new THREE.PointLight( 0xffffff, 0.0, 100 )
+
+var light1 = new THREE.PointLight( 0xffffff, 1, 100);
+light1.position.set(-10, 0, 0)
+scene.add( light1 );
+
+var light2 = new THREE.PointLight( 0xffffff, 1, 100);
+light2.position.set(10, 0, 0)
+scene.add( light2 );
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
 
 
@@ -240,25 +254,6 @@ function draw_circle(theta, phi0, phi1, steps=100) {
 	}
 }
 
-// draw_circle(  PI/4  , 0 , PI )
-// draw_circle( -PI/4  , 0 , PI )
-
-// for (var i = 0; i < 32; i++) {
-// 	const theta = 2*Math.PI * i/32;
-//     update(new THREE.Vector3(Math.cos(theta), 0, Math.sin(theta)));
-// 	addPoint();
-// }
-
-// for (var i = 10; i < 32; i++) {
-// 	const theta = 2*Math.PI * i/32;
-//     update(new THREE.Vector3(Math.cos(theta), 0, Math.sin(theta)));
-// 	addPoint();
-
-//     update(new THREE.Vector3(0.5 * Math.cos(theta), -Math.sqrt(3) / 2, 0.5 * Math.sin(theta)));
-// 	addPoint();
-// }
-
-
 function make_band_geometry(vertices1, vertices2) {
 	const vs = vertices1.concat(vertices2)
 	const m = vertices1.length;
@@ -290,47 +285,40 @@ function make_band_geometry(vertices1, vertices2) {
 		*/
 	}
 
+	geometry.computeFaceNormals();
+	geometry.computeVertexNormals();
 	return geometry;
 }
 
+function index_to_color(i, i_max) {
+	var color = new THREE.Color();
+	color.setHSL(i/i_max/2, 0.5, 0.5);
+	return color
+}
+
 // gets surface of fibers at theta spanning from phi0 to phi1
-function make_fibers_surface_at_theta(theta, phi0, phi1, steps=100) {
+function make_fibers_surface_at_theta(theta, phi0, phi1, color, steps=100) {
 	var fibers = [];
-	var fibers_colors = [];
+	var inset_points = [];
 	for (var i = 0; i < steps; i++) {
 		const phi = phi0 + (i/steps) * (phi1 - phi0);
 		const point = spherical_to_cartesian(1, theta, phi);
-		const color = pointToColor(point);
 		const fiber_vertices = get_fiber_vertices(point);
-		fibers.push(fiber_vertices);
-		fibers_colors.push(color);
 
-		addInsetPointAt(point); // draw point on inset
+		fibers.push(fiber_vertices);
+		inset_points.push(point);
 	}
+
+	addInsetLine(inset_points, color); // draw inset line
 
 	for (var i = 0; i < fibers.length-1; i++) {
 		const fiber0 = fibers[i];
 		const fiber1 = fibers[(i+1)%fibers.length];
-		const color  = fibers_colors[i];
 
-		// convex hull
-
-		var meshMaterial = new THREE.MeshLambertMaterial({ color: color});
-
-		// old - doesn't work since ConvexGeometry fills in the middle
-		// var vertices = fiber0.concat(fiber1);
-		// var meshGeometry = new ConvexGeometry.ConvexBufferGeometry( vertices );
-
-		var meshGeometry = make_band_geometry(fiber0, fiber1);
-
+		const meshMaterial = new THREE.MeshLambertMaterial({ color: color, emissive: 0x222222 });
+		const meshGeometry = make_band_geometry(fiber0, fiber1);
 		var mesh = new THREE.Mesh( meshGeometry, meshMaterial );
-		mesh.material.side = THREE.BackSide; // back faces
-		mesh.renderOrder = 0;
-		scene.add( mesh );
-
-		var mesh = new THREE.Mesh( meshGeometry, meshMaterial.clone() );
-		mesh.material.side = THREE.FrontSide; // front faces
-		mesh.renderOrder = 1;
+		mesh.material.side = THREE.DoubleSide;
 		scene.add( mesh );
 	}
 }
@@ -343,33 +331,28 @@ function make_fibers_surface_at_phi(phi, theta0, theta1, steps=100) {
 		const point = spherical_to_cartesian(1, theta, phi);
 		const color = pointToColor(point);
 		const fiber_vertices = get_fiber_vertices(point);
+		
 		fibers.push(fiber_vertices);
 		fibers_colors.push(color);
-
-		addInsetPointAt(point); // draw point on inset
+		
+		inset_points.push(point);
 	}
+
+	addInsetLine(inset_points, color); // draw inset line
 
 	for (var i = 0; i < fibers.length-1; i++) {
 		const fiber0 = fibers[i];
 		const fiber1 = fibers[(i+1)%fibers.length];
 		const color  = fibers_colors[i];
 
-		// convex hull
-
-		var meshMaterial = new THREE.MeshLambertMaterial({ color: color});
-
-		// old - doesn't work since ConvexGeometry fills in the middle
-		// var vertices = fiber0.concat(fiber1);
-		// var meshGeometry = new ConvexGeometry.ConvexBufferGeometry( vertices );
-
-		var meshGeometry = make_band_geometry(fiber0, fiber1);
-
-		var mesh = new THREE.Mesh( meshGeometry, meshMaterial );
+		const meshMaterial = new THREE.MeshLambertMaterial({ color: color});
+		const meshGeometry = make_band_geometry(fiber0, fiber1);
+		const mesh = new THREE.Mesh( meshGeometry, meshMaterial );
 		mesh.material.side = THREE.BackSide; // back faces
 		mesh.renderOrder = 0;
 		scene.add( mesh );
 
-		var mesh = new THREE.Mesh( meshGeometry, meshMaterial.clone() );
+		mesh = new THREE.Mesh( meshGeometry, meshMaterial.clone() );
 		mesh.material.side = THREE.FrontSide; // front faces
 		mesh.renderOrder = 1;
 		scene.add( mesh );
@@ -382,7 +365,24 @@ for (var i = 0; i < offsets.length; i++) {
 	const theta = PI*thetas[i];
 	const phi0  = -PI*offsets[i];
 	const phi1  = -PI*offsets[i] + PI;
-	make_fibers_surface_at_theta(theta, phi0, phi1, steps=40);
+	const color = index_to_color(i, offsets.length);
+	make_fibers_surface_at_theta(theta, phi0, phi1, color, steps=40);
 }
 
-exports.make_fibers_surface_at_theta = make_fibers_surface_at_theta;
+// draw_circle(  PI/4  , 0 , PI )
+// draw_circle( -PI/4  , 0 , PI )
+
+// for (var i = 0; i < 32; i++) {
+// 	const theta = 2*Math.PI * i/32;
+//     update(new THREE.Vector3(Math.cos(theta), 0, Math.sin(theta)));
+// 	addPoint();
+// }
+
+// for (var i = 10; i < 32; i++) {
+// 	const theta = 2*Math.PI * i/32;
+//     update(new THREE.Vector3(Math.cos(theta), 0, Math.sin(theta)));
+// 	addPoint();
+
+//     update(new THREE.Vector3(0.5 * Math.cos(theta), -Math.sqrt(3) / 2, 0.5 * Math.sin(theta)));
+// 	addPoint();
+// }
